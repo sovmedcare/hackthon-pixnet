d6c572791729ad916453006c9d379ab554d4c6bf
TingHaooo <ztingh@gmail.com>
2019-08-25 17:39:13 +0800

    feat: add refresh and remove item to image selector

--- src/components/ImageSelector.tsx
+++ src/components/ImageSelector.tsx
@@ -1,69 +1,85 @@
-import { Button } from 'antd'
-import { equals, findIndex, flatten, isNil, length, map, reject, update, without } from 'ramda'
+import { Button, Icon } from 'antd'
+import { equals, findIndex, flatten, isNil, length, map, reject, update, concat, none, filter, any, append } from 'ramda'
 import React, { useState } from 'react'
 
-import data from '../../static/food_merged.json'
+import data from '../../static/food_merged_with_id.json'
 import categories from '../../static/categories.json'
 import { getTagsByLabel } from '../getTagsByLabel'
-import { getRandomItem } from '../index'
 
 import './ImageSelector.less'
 
 interface Item {
+  id: string,
   worker: string,
   image_url: string,
   labels: string[]
 }
 interface ImageSelectorContainerProps {
-  items: Item[],
-  setItems: React.Dispatch<React.SetStateAction<Item[]>>,
   setTags: (tags: string[]) => void
 }
 
 interface ImageCardProps {
   item: Item,
-  handleClick: (item: Item) => void
+  handleClick: (item: string) => void
 }
 
 interface SelectedImageCardProps {
   item: Item,
+  handleRemove: (item: string) => void
+}
+
+const getItemsByIds = (ids: string[]): Item[] => filter<Item>((item) => any(equals(item.id), ids), data)
+
+export const getRandomItem = (data: any) => data[Math.floor(Math.random() * data.length)].id
+
+export const getNineItems = (data: any) => {
+  let arr = []
+  while (arr.length !== 9 ) {
+    let item = getRandomItem(data)
+    if (!any(equals(item), arr)) {
+      arr.push(item)
+    }
+  }
+  return arr
 }
 
 const ImageCard = ({item, handleClick}: ImageCardProps) => {
 
-  const { image_url } = item
+  const { image_url, id } = item
 
   return (
-    <div onClick={() => handleClick(item)} style={{ width: '120px', marginTop: '20px' }}>
+    <div onClick={() => handleClick(id)} style={{ width: '120px', marginTop: '20px' }}>
       <img src={image_url} style={{ display: 'bloack', maxWidth: 100, maxHeight: 100, backgroundSize: 'cover' }} />
     </div>
   )
 }
 
-const SelectedImageCard = ({item}: SelectedImageCardProps) => {
-  const { image_url } = item
+const SelectedImageCard = ({ item, handleRemove }: SelectedImageCardProps) => {
+  const { image_url, id } = item
   return (
-    <div style={{ width: '120px', marginTop: '20px' }}>
+    <div style={{ width: '130px', marginTop: '20px', position: 'relative', padding: '15px' }}>
       <img src={image_url} style={{ display: 'bloack', maxWidth: 100, maxHeight: 100, backgroundSize: 'cover' }} />
+      <Icon type="close" onClick={() => handleRemove(id)} style={{position: 'absolute', top: 0, right: 0 }} />
     </div>
   )
 }
 
 export const ImageContainer = ({
-  items,
-  setItems,
   setTags
 }: ImageSelectorContainerProps) => {
-  const [ selectedImages, setSelectedImages ] = useState<Item[]>([])
+  const [ nineItems, setNineItems ] = useState<string[]>(getNineItems(data))
+  const [ selectedItems, setSelectedItems ] = useState<string[]>([])
+  const [ abandonedItems, setAbandonedItems ] = useState<string[]>([])
 
-  const handleClick = (item: Item) => {
-    if (length(selectedImages) === 3) {
+  const handleClick = (item: string) => {
+    if (length(selectedItems) === 3) {
       return
     }
-    setSelectedImages(state => [...state, item])
-    setItems(state => {
+    setSelectedItems(append(item, selectedItems))
+    setNineItems(state => {
       const orginItems = state
-      const newItem = getRandomItem(without([...orginItems, ...selectedImages], data))
+      const filteredData = filter((data: Item) => none(equals(data.id), [...nineItems, ...selectedItems, ...abandonedItems]), data)
+      const newItem = getRandomItem(filteredData)
       const replaceItemIndex = findIndex(equals(item), orginItems)
       const newNineItems = update(replaceItemIndex, newItem, orginItems)
       return newNineItems
@@ -71,11 +87,10 @@ export const ImageContainer = ({
   }
 
   const handleSubmit = async () => {
-    const rawLabels = flatten(map(image => image.labels, selectedImages))
-    console.log(`rawLabels`, rawLabels);
+    const rawLabels = flatten(map(image => image.labels, getItemsByIds(selectedItems)))
     const pixnetTagsList: string[][] = await Promise.all(
       map(label => getTagsByLabel(label), rawLabels)
-    )
+      )
     const pixnetTags = reject(isNil, flatten(pixnetTagsList))
     const suggestedTags = reject(isNil, map(pixnetTag => categories[pixnetTag], pixnetTags))
 
@@ -83,25 +98,32 @@ export const ImageContainer = ({
   }
 
   const handleRefresh = () => {
-    setSelectedImages([])
+    setAbandonedItems([...abandonedItems, ...nineItems])
+    const newNimeItems = getNineItems(filter<Item>(item => none(equals(item.id), [...abandonedItems, ...nineItems, ...selectedItems]), data))
+    setNineItems(newNimeItems)
+  }
+
+  const handleRemove = (item: string) => {
+    setSelectedItems(reject(equals(item), selectedItems))
+    setAbandonedItems(append(item, abandonedItems))
   }
 
 
   return (
     <div className="image-selector">
       <h3>請在下面選出一張你喜歡的圖片</h3>
-      <div className="image-list" style={{ display: 'flex', width: '360px', flexWrap: 'wrap' }}>
-        {map((item: Item) => <ImageCard key={item.image_url} handleClick={handleClick} item={item}/>, items)}
-      </div>
-      <h3>你喜歡的圖片 {`${length(selectedImages)} / 3`}</h3>
-      <div className="selected-image-list" style={{ display: 'flex', width: '360px', flexWrap: 'wrap' }}>
-        {map((item: Item) => <SelectedImageCard key={item.image_url} item={item}/>, selectedImages)}
-      </div>
-      <Button type='primary' disabled={length(selectedImages) !== 3} onClick={handleSubmit}>
-        開始推薦文章
+      <Button type='default' style={{ marginLeft: '10px' }} disabled={length(abandonedItems) === (250-12)} onClick={handleRefresh}>
+        重整所有圖片
       </Button>
-      <Button type='default' style={{ marginLeft: '10px' }} disabled={length(selectedImages) !== 3} onClick={handleRefresh}>
-        重新選擇
+      <div className="image-list" style={{ display: 'flex', width: '360px', flexWrap: 'wrap' }}>
+        {map((item: Item) => <ImageCard key={item.image_url} handleClick={handleClick} item={item}/>, getItemsByIds(nineItems))}
+      </div>
+      <h3>你喜歡的圖片 {`${length(selectedItems)} / 3`}</h3>
+      <div className="selected-image-list" style={{ display: 'flex', width: '390px', flexWrap: 'wrap' }}>
+        {map((item: Item) => <SelectedImageCard key={item.image_url} item={item} handleRemove={handleRemove}/>, getItemsByIds(selectedItems))}
+      </div>
+      <Button type='primary' disabled={length(selectedItems) !== 3} onClick={handleSubmit}>
+        開始推薦文章
       </Button>
     </div>
   )
